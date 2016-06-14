d00a58517ec576bbbc21fc7096864b44ebd7e046
manlan <manlan2010@163.com>
2016-06-15 01:54:48 +0800

    Initial Commit

---
+++ empty1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  empty1
+//
+//  Created by Mac on 16/6/15.
+//  Copyright (c) 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
